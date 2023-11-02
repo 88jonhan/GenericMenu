f93d2d3021702a791bc0c46f790fc2a21c04afee
88jonhan <88jonhan@skola.boras.se>
2023-11-02 19:35:19 +0100

    påbärjat sidhantering

--- src/MenuRepository.cs
+++ src/MenuRepository.cs
@@ -26,30 +26,94 @@ class MenuRepository
     /// <param name="showNumbers">Bool to control wether or not prefix-numbers should be written out before the item</param>
     /// <param name="brackets">The format of the prefix numbers.</param>
     /// <returns>The chosen object from the list.</returns>
-    public static void UpdateMenu<T>(int activeItem, List<T> listToShow, bool showNumbers, string brackets)
+    /// 
+    //pagesData = (totalPages, activePage, itemsPerPage, totalItems);
+    public static void UpdateMenu<T>(int activeItem, (int, int, int, int) pagesData, List<T> listToShow, bool showNumbers, string brackets)
     {
+        int totalPages = pagesData.Item1;
+        int activePage = pagesData.Item2;
+        int itemsPerPage = pagesData.Item3;
+        int totalItems = pagesData.Item4;
+
         int iteration = 1;
-        foreach (T item in listToShow)
+
+        if (totalPages == 1)
         {
-            if (CheckIfActive(activeItem, iteration))
+            foreach (T item in listToShow)
             {
-                Console.BackgroundColor = ConsoleColor.DarkGray;
-                if (showNumbers)
+                if (CheckIfActive(activeItem, iteration))
                 {
-                    Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    if (showNumbers)
+                    {
+                        Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
+                    }
+                    Console.WriteLine(item);
+                    Console.ResetColor();
                 }
-                Console.WriteLine(item);
-                Console.ResetColor();
+                else
+                {
+                    if (showNumbers)
+                    {
+                        Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
+                    }
+                    Console.WriteLine(item);
+                }
+                iteration++;
+            }
+        }
+        else
+        {
+            int checkIfLastItem = 0;
+            int currentItem = 0;
+            int checkIfLastItemOnPage = 0;
+
+
+            while (checkIfLastItem < totalItems)
+            {
+                while (checkIfLastItemOnPage < itemsPerPage)
+                {
+                    Console.WriteLine(listToShow[currentItem]);
+                    checkIfLastItemOnPage++;
+                    currentItem++;
+                }
+                checkIfLastItem++;
+            }
+        }
+        Console.WriteLine(activePage + "/" + totalPages);
+    }
+
+    public static int GetItemsPerPage(int totalItems)
+    {
+        int itemsPerPage;
+        if (Console.WindowHeight <= 10)
+        {
+            itemsPerPage = 1;
+        }
+        else
+        {
+            itemsPerPage = Console.WindowHeight - 10;
+        }
+        return itemsPerPage;
+    }
+
+    public static int GetTotalPages(int itemsPerPage, int totalItems)
+    {
+        if (totalItems < itemsPerPage)
+        {
+            return 1;
+        }
+        else
+        {
+            if (totalItems % itemsPerPage == 0)
+            {
+                return totalItems / itemsPerPage;
             }
             else
             {
-                if (showNumbers)
-                {
-                    Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
-                }
-                Console.WriteLine(item);
+                int remainder = totalItems % itemsPerPage;
+                return (totalItems - remainder) / itemsPerPage;
             }
-            iteration++;
         }
     }
 }
