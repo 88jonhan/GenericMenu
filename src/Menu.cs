f131f97b5813c218ba93c52a4394efdf4a21f47b
88jonhan <88jonhan@skola.boras.se>
2023-11-02 13:30:17 +0100

    some pageination code, but not so good

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.Runtime.Serialization;
+
 namespace GenericMenu;
 
 public class Menu
@@ -29,13 +32,18 @@ public class Menu
     /// <returns>The chosen object from the list, or default(T) if no item were chosen</returns>
     public static T ShowMenuAndCheckUserInput<T>(List<T> listToShow, bool showNumbers, string brackets)
     {
+        int cursorTop = Console.CursorTop;
+        int activePage = 1;
+
         bool menuActive = true;
         int activeItem = 1;
         while (menuActive)
         {
             Console.Clear();
-            MenuRepository.UpdateMenu(activeItem, listToShow, showNumbers, brackets);
-            UI.WriteMenuInstructions();
+            (int, int) pagesAndRemainder = TotalPages(listToShow.Count, Console.WindowHeight);
+            UpdateMenu(activeItem, listToShow, showNumbers, brackets, activePage, pagesAndRemainder);
+            //UI.WriteMenuInstructions();
+
             ConsoleKeyInfo key = Console.ReadKey();
 
             switch (key.Key)
@@ -49,7 +57,6 @@ public class Menu
                     {
                         activeItem--;
                     }
-                    MenuRepository.UpdateMenu(activeItem, listToShow, showNumbers, brackets);
                     break;
                 case ConsoleKey.DownArrow:
                     if (activeItem == listToShow.Count)
@@ -60,7 +67,18 @@ public class Menu
                     {
                         activeItem++;
                     }
-                    MenuRepository.UpdateMenu(activeItem, listToShow, showNumbers, brackets);
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (activePage != pagesAndRemainder.Item1)
+                    {
+                        activePage++;
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (activePage != 1)
+                    {
+                        activePage--;
+                    }
                     break;
                 case ConsoleKey.Enter:
                     return listToShow[activeItem - 1];
@@ -70,8 +88,141 @@ public class Menu
                 default:
                     continue;
             }
+
         }
+
         return default;
     }
 
+    public static void UpdateMenu<T>(int activeItem, List<T> listToShow, bool showNumbers, string brackets, int activePage, (int, int) pagesAndRemainder)
+    {
+        int windowHeight = Console.WindowHeight;
+        int windowWidth = Console.WindowWidth;
+
+        int cursorLeft = Console.CursorLeft;
+        int cursorTop = Console.CursorTop;
+        int rowsPerPage = 4;
+        int iteration = 1;
+        int currentPage = 1;
+
+        if (listToShow.Count > rowsPerPage)
+        {
+            while (currentPage <= rowsPerPage)
+            {
+                if (currentPage == 1)
+                {
+                    Console.SetCursorPosition(cursorLeft, cursorTop);
+                }
+                else
+                {
+                    Console.SetCursorPosition(cursorLeft, Console.CursorTop);
+                }
+
+                for (int j = 1; j < windowWidth; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                if (currentPage == 1)
+                {
+                    Console.SetCursorPosition(cursorLeft, cursorTop);
+                }
+                else
+                {
+                    Console.SetCursorPosition(cursorLeft, Console.CursorTop);
+                }
+
+                if (MenuRepository.CheckIfActive(activeItem, iteration))
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    if (showNumbers)
+                    {
+                        Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
+                    }
+                    Console.WriteLine(listToShow[i]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    if (showNumbers)
+                    {
+                        Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
+                    }
+                    Console.WriteLine(listToShow[i]);
+                }
+                iteration++;
+            }
+            Console.WriteLine($"\nSida {activePage}/{pagesAndRemainder.Item1}");
+            Console.WriteLine(rowsPerPage + ", " + Console.WindowHeight);
+        }
+        else
+        {
+            foreach (var item in listToShow)
+            {
+                if (iteration == 1)
+                {
+                    Console.SetCursorPosition(cursorLeft, cursorTop);
+                }
+                else
+                {
+                    Console.SetCursorPosition(cursorLeft, Console.CursorTop);
+                }
+
+                for (int j = 1; j < windowWidth; j++)
+                {
+                    Console.Write(" ");
+                }
+                if (iteration == 1)
+                {
+                    Console.SetCursorPosition(cursorLeft, cursorTop);
+                }
+                else
+                {
+                    Console.SetCursorPosition(cursorLeft, Console.CursorTop);
+                }
+                if (MenuRepository.CheckIfActive(activeItem, iteration))
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    if (showNumbers)
+                    {
+                        Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
+                    }
+                    Console.WriteLine(item);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    if (showNumbers)
+                    {
+                        Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
+                    }
+                    Console.WriteLine(item);
+                }
+                iteration++;
+            }
+        }
+    }
+
+
+    public static (int, int) TotalPages(int totalItems, int windowHeight)
+    {
+        (int, int) pagesAndRemainder;
+        int pages = 1;
+        int rows = 4;
+        int remainder = 0;
+        if (totalItems > rows)
+        {
+            if (totalItems % rows == 0)
+            {
+                pages = totalItems / rows;
+            }
+            else
+            {
+                remainder = totalItems % rows;
+                pages = (totalItems - remainder) / rows;
+            }
+        }
+        pagesAndRemainder = (pages, remainder);
+        return pagesAndRemainder;
+    }
 }
--- src/MenuRepository.cs
+++ src/MenuRepository.cs
@@ -18,38 +18,4 @@ class MenuRepository
         return isActive;
     }
 
-    /// <summary>
-    /// Updates the menu based on user inputs
-    /// </summary>
-    /// <param name="activeItem">Integer which represents the current active item</param>
-    /// <param name="listToShow">The list to use as menu.</param>
-    /// <param name="showNumbers">Bool to control wether or not prefix-numbers should be written out before the item</param>
-    /// <param name="brackets">The format of the prefix numbers.</param>
-    /// <returns>The chosen object from the list.</returns>
-    public static void UpdateMenu<T>(int activeItem, List<T> listToShow, bool showNumbers, string brackets)
-    {
-        int iteration = 1;
-        foreach (T item in listToShow)
-        {
-            if (CheckIfActive(activeItem, iteration))
-            {
-                Console.BackgroundColor = ConsoleColor.DarkGray;
-                if (showNumbers)
-                {
-                    Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
-                }
-                Console.WriteLine(item);
-                Console.ResetColor();
-            }
-            else
-            {
-                if (showNumbers)
-                {
-                    Console.Write($"{brackets.Replace("T", $"{iteration}")} ");
-                }
-                Console.WriteLine(item);
-            }
-            iteration++;
-        }
-    }
 }
